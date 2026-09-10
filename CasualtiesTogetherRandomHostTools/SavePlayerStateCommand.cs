0f30ea53c705af4af8dd10b0b00d6e0fe4f4a883
Voxeles <owca.pospolita@gmail.com>
2026-09-10 10:13:08 +0200

    Replace spaces with underscores

--- CasualtiesTogetherRandomHostTools/SavePlayerStateCommand.cs
+++ CasualtiesTogetherRandomHostTools/SavePlayerStateCommand.cs
@@ -170,12 +170,7 @@ public static class SavePlayerStateCommand
         Con.RegisterCommand(comm);
     }
 
-    private static readonly char[] InvalidChars = [
-        '<', '>', ':', '"', '/', '\\', '|', '?', '*',
-        '\u0000', '\u0001', '\u0002', '\u0003', '\u0004', '\u0005', '\u0006', '\u0007',
-        '\u0008', '\u0009', '\u000A', '\u000B', '\u000C', '\u000D', '\u000E', '\u000F',
-        '\u0010', '\u0011', '\u0012', '\u0013', '\u0014', '\u0015', '\u0016', '\u0017',
-        '\u0018', '\u0019', '\u001A', '\u001B', '\u001C', '\u001D', '\u001E', '\u001F'];
+    private static readonly HashSet<char> InvalidChars = [' ', '<', '>', ':', '"', '/', '\\', '|', '?', '*'];
 
     public static string SanitizePlayerName(string name)
     {
@@ -184,7 +179,7 @@ public static class SavePlayerStateCommand
 
         var sb = new StringBuilder(name.Length);
         foreach (var c in name)
-            sb.Append(Array.IndexOf(InvalidChars, c) >= 0 ? '_' : c);
+            sb.Append(c <= '\u001F' || InvalidChars.Contains(c) ? '_' : c);
 
         return sb.ToString();
     }
